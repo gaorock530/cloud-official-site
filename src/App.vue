bb28a97deb3559d358116e4f5b314ddd21b5e8d9
magic/gaoce <gaoce@beyondcent.com>
2021-12-11 18:22:35 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+
+<template>
+  <el-container>
+    <Header />
+    <Main>
+      <div class="test">
+        {{a}}
+        <img alt="Vue logo" src="./assets/logo.png" />
+        <el-button>I am ElButton</el-button>
+      </div>
+    </Main>
+
+    <el-footer class="bg">Footer</el-footer>
+  </el-container>
+
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+import Main from '@/components/Main.vue'
+
+export default {
+  components: { Header, Main },
+  data() {
+    return {
+      a: 'old text'
+    }
+  }
+  // setup() {
+  //   return {
+  //     a: 'text'
+  //   }
+  // },
+}
+</script>
+
+<style lang="scss">
+
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.test {
+  height: 1000px
+}
+.bg {
+  background-color: #2c3e50;
+  color: #fff;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>header</header>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    return {
+      a: 1
+    }
+  }
+}
+
+</script>
+
+
+<style lang="scss">
+
+header {
+  position: fixed;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  height: var(--header-height);
+  background-color: #159592;
+  color: #fff
+}
+
+</style>
